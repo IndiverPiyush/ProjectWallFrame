efb9a70947d810372fe53ff4cfd32b6c4ce9db01
Indiver Piyush <indiverpiyush.ip@gmail.com>
2024-11-27 23:14:41 +0530

    added routing, login and topbar component

---
+++ wall-frame/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+
+const routes: Routes = [{ path: 'login', component: LoginComponent }];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes), CommonModule], // Ensure it's RouterModule.forRoot
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
